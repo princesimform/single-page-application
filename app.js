a3d7a765453398e463ebfebc98efa17fc842dd08
princesimform <prince.m@simformsolutions.com>
2023-03-16 16:21:38 +0530

    SPA design complete

--- app.js
+++ app.js
@@ -6,3 +6,42 @@ function toggleNav() {
         document.getElementById("sidebar").style.width = "95%";
     }
 }
+
+
+// Header Position
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currrenPos = window.pageYOffset;
+    if (prevScrollpos > currrenPos || currrenPos > 3800) {
+        document.getElementById('header').style.top = "0";
+        document.getElementById('header').style.boxShadow = "0 0 1.5rem #00000033";
+        document.getElementById('header-container').style.border = "none";
+    } else {
+        document.getElementById('header').style.top = "-80px";
+    }
+
+    if (window.pageYOffset == 0) {
+        document.getElementById('header').style.boxShadow = "none"
+        document.getElementById('header-container').style.borderBottom = '1px solid #9ea5a5'
+    }
+
+
+    prevScrollpos = currrenPos;
+}
+
+
+function active(positionName) {
+
+    let btns = document.querySelectorAll('.menu-item');
+    console.log(btns);
+    btns.forEach((element, inx) => {
+        if (element.classList.contains(positionName)) {
+            element.classList.add('active-section')
+        } else {
+            element.classList.remove('active-section')
+        }
+    });
+    document.getElementById("sidebar").style.width = "0%";
+    document.getElementById("sidebar").classList.toggle("active");
+}
+
